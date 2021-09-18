fe670a6df8c3df8d01d20f8c7b106c0196f20cf6
farita1699 <uwanjerry25@gmail.com>
2021-09-18 13:49:31 -0400

    cartegories

--- project/api.py
+++ project/api.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets, permissions
-from .serializers import UserSerializer
+from project.models import Cartegory
+from rest_framework import viewsets, permissions, generics
+from .serializers import UserSerializer, CartegorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from project import serializers
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -8,3 +13,25 @@ class UserViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = UserSerializer
+
+class CartegoryViewSet(viewsets.ModelViewSet):
+    queryset = Cartegory.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = CartegorySerializer
+
+# class CreateUserView(APIView):
+#     serializer_class = CreateUserSerializer
+
+#     def post(self, request, format=None):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             username = serializer.data.username
+#             password = serializer.data.password
+
+#         user = User(username=username, password=password)
+#         user.save()
+
+
+
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from project import views
 from rest_framework import routers
-from .api import UserViewSet
+from .api import UserViewSet, CartegoryViewSet
 
 router = routers.DefaultRouter()
 router.register('api/users', UserViewSet, 'users')
+router.register('api/categories', CartegoryViewSet, 'cartegory')
 
 urlpatterns = router.urls
 
