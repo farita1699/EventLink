abf9ca960eb6a56535057d75bfcb587359433157
farita1699 <uwanjerry25@gmail.com>
2021-09-18 20:57:32 -0400

    added more fields

--- project/models.py
+++ project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -14,8 +14,13 @@ class Cartegory(models.Model):
 
 class Events(models.Model):
     cartegory = models.ForeignKey('Cartegory', on_delete=models.CASCADE, related_name='events')
+    #hosts = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    participants = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     title = models.CharField(max_length=100)
     description = models.TextField()
+    location = models.CharField(max_length=50)
+    isVirtual = models.BooleanField()
+
 
     def __str__(self):
         return self.title
